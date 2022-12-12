a6de8e241bcd1f701a18b83630a3769df4ba5f8f
MaxRem <mxremez77@gmail.com>
2022-12-12 19:07:02 +0200

    contact component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Activities from './components/Activities';
 import Booking from './components/Booking';
+import Contact from './components/Contact';
 import Gallery from './components/Gallery';
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
@@ -15,6 +16,7 @@ function App() {
 			<Activities />
 			<Booking />
 			<Gallery />
+			<Contact />
 		</div>
 	);
 }
