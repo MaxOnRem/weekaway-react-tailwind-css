42c292b9173da2b1de9802b02010e66ec7b5eca6
MaxRem <mxremez77@gmail.com>
2022-12-12 18:12:31 +0200

    updated Activities

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,36 +1,6 @@
 const Activities = () => {
 	return (
 		<section className="max-w-[1140px] m-auto w-full md:flex mt-[-75px]">
-			{/* <div className="relative p-4">
-				<h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">
-					Resorts
-				</h3>
-				<img
-					src="https://images.unsplash.com/photo-1548107121-ba49955415b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTV8fHJlc29ydHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-					alt="resorts"
-					className="w-full h-full object-cover relative border-4 border-white shadow-lg"
-				/>
-			</div>
-			<div className="relative p-4">
-				<h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">
-					Cruise
-				</h3>
-				<img
-					src="https://images.unsplash.com/photo-1518650310565-c782d7ff666e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNydWlzZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-					alt="resorts"
-					className="w-full h-full object-cover relative border-4 border-white shadow-lg"
-				/>
-			</div>
-			<div className="relative p-4">
-				<h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">
-					Excursions
-				</h3>
-				<img
-					src="https://images.unsplash.com/photo-1544551763-46a013bb70d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-					alt="resorts"
-					className="w-[500px] h-[250px] object-cover relative border-4 border-white shadow-lg"
-				/>
-			</div> */}
 			<div className="relative p-4">
 				<h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">
 					Resorts
